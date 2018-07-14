77e4ca946205e60d1e36bd87bd852e824a65d857
PVillela <pvillela@gmail.com>
2018-07-14 07:38:46 -0400

    Converted tabs to space in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,36 +10,36 @@ buildscript {
 }
 
 plugins {
-	application
-	id("org.jetbrains.kotlin.jvm") version "1.2.51"
+    application
+    id("org.jetbrains.kotlin.jvm") version "1.2.51"
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 application {
-	mainClassName = "examples.KotlinExampleKt"
+    mainClassName = "examples.KotlinExampleKt"
 }
 
 tasks {
-	withType<KotlinCompile> {
-		kotlinOptions {
-			jvmTarget = "1.8"
-			freeCompilerArgs = listOf("-Xjsr305=strict")
-		}
-	}
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
 }
 
 val test by tasks.getting(Test::class) {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	implementation("ch.qos.logback:logback-classic:1.0.13")
+    implementation("ch.qos.logback:logback-classic:1.0.13")
 
     implementation("io.projectreactor:reactor-core:3.1.8.RELEASE")
 
@@ -47,10 +47,10 @@ dependencies {
     implementation("io.reactivex:rxjava-reactive-streams:1.2.1")  // used by Hystrix
 
     testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.assertj:assertj-core")
+    testImplementation("org.assertj:assertj-core")
 
-	testImplementation("org.junit.jupiter:junit-jupiter-api")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 val compileKotlin: KotlinCompile by tasks
